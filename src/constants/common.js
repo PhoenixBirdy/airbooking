d7fac1375caf00de2027860cf635e590829bcdc1
PhoenixSunday <rusbird82@gmail.com>
2023-06-16 12:36:39 +0300

    Add constants

---
+++ src/constants/common.js
@@ -0,0 +1,38 @@
+const DEPARTURE_DATE = 'departureDate';
+const DEPARTURE_AIRPORT = 'departureAirport';
+const ARRIVAL_AIRPORT = 'arrivalAirport';
+const DEPARTURE_DATE_DEFAULT = '2023-06-10';
+const DEFAULT_FORM_VALUES = {
+  [DEPARTURE_AIRPORT]: '',
+  [ARRIVAL_AIRPORT]: '',
+  [DEPARTURE_DATE]: DEPARTURE_DATE_DEFAULT,
+};
+const FROM = 'from';
+const TO = 'to';
+const MAX_LENGTH = 1000;
+const MEDIA_MIN_WIDTH_800 = '(min-width: 800px)';
+const FULL_WIDTH = '100vw';
+const HALF_WIDTH = '50vw';
+const FULL_HEIGHT = '100vh';
+const CUSTOM_WIDTH_320 = '320px';
+const CUSTOM_HEIGHT_500 = '500px';
+const COUNT_5 = 5;
+const SECONDS_1 = 1000;
+
+export {
+  FULL_HEIGHT,
+  DEPARTURE_DATE,
+  DEPARTURE_AIRPORT,
+  ARRIVAL_AIRPORT,
+  DEFAULT_FORM_VALUES,
+  FROM,
+  TO,
+  MAX_LENGTH,
+  MEDIA_MIN_WIDTH_800,
+  FULL_WIDTH,
+  HALF_WIDTH,
+  CUSTOM_WIDTH_320,
+  COUNT_5,
+  SECONDS_1,
+  CUSTOM_HEIGHT_500,
+};
---
+++ src/constants/errors.js
@@ -0,0 +1,5 @@
+const LATIN_TEXT_ERROR = 'Enter text in latin';
+const GENDER_SELECT_ERROR = 'Select gender';
+const DIGITS_ERROR = 'Enter digits';
+
+export { LATIN_TEXT_ERROR, GENDER_SELECT_ERROR, DIGITS_ERROR };
