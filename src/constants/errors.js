ed0a8f42ec443b6dc49145f01a9875215965f351
PhoenixSunday <rusbird82@gmail.com>
2023-06-16 12:45:39 +0300

    take out  error constant

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,5 +1,12 @@
 const LATIN_TEXT_ERROR = 'Enter text in latin';
 const GENDER_SELECT_ERROR = 'Select gender';
 const DIGITS_ERROR = 'Enter digits';
+const FETCH_MESSAGE_ERROR =
+  "Can't fetch data from server! - (EMULATED ERROR 30%)";
 
-export { LATIN_TEXT_ERROR, GENDER_SELECT_ERROR, DIGITS_ERROR };
+export {
+  LATIN_TEXT_ERROR,
+  GENDER_SELECT_ERROR,
+  DIGITS_ERROR,
+  FETCH_MESSAGE_ERROR,
+};
